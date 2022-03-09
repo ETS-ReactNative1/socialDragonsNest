89fa7827445b29c2d7f176fb2e70331a4d27b403
Akriti Keswani <53410126+AkritiKeswani@users.noreply.github.com>
2022-03-08 23:18:02 -0500

    Update dbschema.js

--- dbschema.js
+++ dbschema.js
@@ -14,7 +14,7 @@ let db = {
     posts: [
       {
         userHandle: 'user',
-        body: 'This is a sample scream',
+        body: 'This is a sample post',
         createdAt: '2019-03-15T10:59:52.798Z',
         likeCount: 5,
         commentCount: 3
